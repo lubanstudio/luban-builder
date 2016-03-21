29cb39052009ffd25e2cf329414a9a1309c75e9b
Unknwon <u@gogs.io>
2016-03-21 19:02:25 -0400

    Able to update matrix info

---
+++ main.go
@@ -0,0 +1,65 @@
+// Copyright 2016 Unknwon
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/Unknwon/com"
+	"github.com/parnurzeal/gorequest"
+)
+
+const APP_VER = "0.0.1.0321"
+
+func main() {
+	log.Info("Luban Builder ", APP_VER)
+
+	if len(EndPoint) == 0 {
+		fmt.Print("Please enter the server end point: ")
+		EndPoint = AskLine()
+		SaveEndPoint()
+	}
+
+	if len(Token) == 0 {
+		fmt.Print("Please enter the token given by the admin: ")
+		Token = AskLine()
+		SaveToken()
+	}
+
+	matricesFile := "matrices.json"
+	if !com.IsFile(matricesFile) {
+		log.Fatalf("File '%s' not found, please define it first.", matricesFile)
+	}
+
+	var err error
+	MatricesData, err = ioutil.ReadFile(matricesFile)
+	if err != nil {
+		log.Fatalf("Fail to load '%s': %v", matricesFile)
+	}
+
+	resp, _, errs := gorequest.New().Post(EndPoint+"/builder/matrix").
+		Set("X-LUBAN-TOKEN", Token).SendString(string(MatricesData)).End()
+	if len(errs) > 0 {
+		log.Fatalf("Fail to update matrix info: %v", errs[0])
+	}
+	if resp.StatusCode != 204 {
+		log.Fatalf("Unexpected response status '%d' for updating matrix info.", resp.StatusCode)
+	}
+
+	log.Info("All going well, start heart beating...")
+	Heartbeating()
+}
---
+++ setting.go
@@ -0,0 +1,64 @@
+// Copyright 2016 Unknwon
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/Unknwon/com"
+	"gopkg.in/ini.v1"
+)
+
+var (
+	EndPoint string
+	Token    string
+
+	MatricesData []byte
+
+	ConfFile = ".luban.ini"
+	Cfg      *ini.File
+)
+
+func init() {
+	if !com.IsFile(ConfFile) {
+		Cfg = ini.Empty()
+		return
+	}
+
+	var err error
+	Cfg, err = ini.Load(ConfFile)
+	if err != nil {
+		log.Fatalf("Fail to load '%s': %v", ConfFile, err)
+	}
+
+	sec := Cfg.Section("")
+	EndPoint = sec.Key("END_POINT").String()
+	Token = sec.Key("TOKEN").String()
+}
+
+func SaveSettings() {
+	if err := Cfg.SaveTo(ConfFile); err != nil {
+		log.Fatalf("Fail to save '%s': %v", ConfFile, err)
+	}
+}
+
+func SaveEndPoint() {
+	Cfg.Section("").Key("END_POINT").SetValue(EndPoint)
+	SaveSettings()
+}
+
+func SaveToken() {
+	Cfg.Section("").Key("TOKEN").SetValue(Token)
+	SaveSettings()
+}
